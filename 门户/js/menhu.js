553fca5eafc4183a80423714a33c2836feaf1634
liqiue <1827728033@qq.com>
2018-06-28 08:11:40 +0800

    门户

---
+++ 门户/js/menhu.js
@@ -0,0 +1,12 @@
+
+//----swiper轮播图效果-----
+  var mySwiper = new Swiper ('.banner', {
+    autoplay:true,//自动轮播效果
+    loop: true,//手动轮播效果
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    
+  })  
+  
